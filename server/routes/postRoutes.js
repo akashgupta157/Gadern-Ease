ce65f9075a0f9eb9907e54bc5ee64fe2ab903bdd
Biswajit Roy <broy16431@gmail.com>
2023-08-23 23:35:07 +0530

    crud operations done

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,71 @@
+const express=require("express");
+const postModel = require("../models/post.model");
+const postRouter=express.Router();
+
+
+postRouter.get("/",async(req,res)=>{
+    try {
+        const posts=await postModel.find();
+        res.status(200).send(posts)
+    } catch (error) {
+        res.status(500).send({"error":"Internal Server Error"})
+    }
+})
+
+postRouter.get("/:id",async(req,res)=>{
+    const {id}=req.params;
+    try {
+        const post=await postModel.findOne({_id:id});
+        res.status(200).send(post)
+    } catch (error) {
+        res.status(500).send({"error":"Internal Server Error"})
+    }
+})
+
+postRouter.post("/add",async(req,res)=>{
+    try {
+        const newPost=new postModel(req.body);
+        await newPost.save();
+        res.status(201).send({'msg':"New Post has been created"})
+    } catch (error) {
+        res.status(500).send({"error":"Internal Server Error"})
+    }
+})
+
+postRouter.patch("/update/:id",async(req,res)=>{
+    const {id}=req.params;
+    const post=await postModel.findOne({_id:id})
+    try {
+        if(req.body.username!==post.username){
+            res.send({"msg":"You are not authorizes to make changes"})
+        }else{
+            await postModel.findByIdAndUpdate({_id:id},{...req.body})
+            res.status(201).send({"msg":`Post with _id:${id} has been updated`})
+        }
+    } catch (error) {
+        res.status(500).send({"error":"Internal Server Error"})
+    }
+})
+
+
+postRouter.delete("/delete/:id",async(req,res)=>{
+    const {id}=req.params;
+    const post=await postModel.findOne({_id:id})
+    try {
+        if(req.body.username!==post.username){
+            res.send({"msg":"You are not authorizes to make changes"})
+        }else{
+            await postModel.findByIdAndDelete({_id:id})
+            res.status(200).send({"msg":`Post with _id:${id} has been deleted`})
+        }
+    } catch (error) {
+        res.status(500).send({"error":"Internal Server Error"})
+    }
+})
+
+
+
+
+
+
+module.exports={postRouter}
