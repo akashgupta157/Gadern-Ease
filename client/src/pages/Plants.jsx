055d69086cc50eed5a2d1f03063145aa0d9679b8
Juri Kalita <jurikalita011@gmail.com>
2023-08-25 00:33:09 +0530

    added day3 tasks

--- client/src/pages/Plants.jsx
+++ client/src/pages/Plants.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import {
-  Box,
-  Text,
-  VStack,
-  HStack,
-  Button,
-  Flex,
-  Image,
-} from "@chakra-ui/react";
+import { Box, Text, VStack, Button, Flex, Image } from "@chakra-ui/react";
 export const Plants = () => {
+  const textStyle = {
+    mt: "0px",
+    p: "0px",
+    fontSize: "14px",
+    fontFamily: "Roboto(woff2),sans-serif",
+    lineHeight: "17px",
+    verticalAlign: "baseline",
+    fontWeight: 700,
+  };
+  const btnStyle = {
+    background: "none",
+    cursor: "pointer",
+    border: "none",
+    _hover: {
+      backgroundColor: "none",
+    },
+  };
   return (
     <Box border={"2px solid red"}>
       <Text
         textAlign={"center"}
-        display={"block"}
         font={"Roboto"}
         color={"#6DB644"}
         lineHeight={"67.2px"}
@@ -24,309 +32,224 @@ export const Plants = () => {
       >
         Plants
       </Text>
-      <Box margin={"auto"}>
-        <Flex alignItems={"center"} justifyContent={"center"} gap={5}>
-          <HStack>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
+      <Box margin={"auto"} mt={"80px"} mb={"20px"}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Button style={btnStyle}>
+            <VStack spacing={0}>
+              <Box as="span" title="Aquatic plants">
                 <Image
                   m={"0px"}
                   p={"0px"}
                   width={"45px"}
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTny5FxOIt6HYOpr71Mc2zr9eLXhChHxlZFQg&usqp=CAU"
                 />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Aquatic plants
-                </Text>
               </Box>
+
+              <Text style={textStyle}>Aquatic plants</Text>
+            </VStack>
+          </Button>
+
+          <VStack spacing={5}>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Balcony flowers">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"40px"}
+                    src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR6YSoOBHJYV8h7Ztys1faALIK88Wb3ImuX6xn2nF8J_QTTtWeA"
+                  />
+                </Box>
+                <Text style={textStyle}>Balcony Flowers</Text>
+              </VStack>
             </Button>
-          </HStack>
-          <VStack spacing={4}>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"40px"}
-                  src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR6YSoOBHJYV8h7Ztys1faALIK88Wb3ImuX6xn2nF8J_QTTtWeA"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Balcony Flowers
-                </Text>
-              </Box>
-            </Button>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"30px"}
-                  src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSs8NB0gSCNfqS27QUGq9fN-9payxYv_2wHvEcTLr4_th854IDD"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Ornamental grasses
-                </Text>
-              </Box>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Ornamental grasses">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"30px"}
+                    src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSs8NB0gSCNfqS27QUGq9fN-9payxYv_2wHvEcTLr4_th854IDD"
+                  />
+                </Box>
+                <Text style={textStyle}>Ornamental grasses</Text>
+              </VStack>
             </Button>
           </VStack>
-          <VStack spacing={4}>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"40px"}
-                  src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQQrN5nB6JGSC44tcUEhMvy0nMKJBY5N2_gyjW4pQ96aXO_jJAZ"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Bulbous and tuberous plants
-                </Text>
-              </Box>
+          <VStack spacing={5}>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Bulbous plants">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"40px"}
+                    src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQQrN5nB6JGSC44tcUEhMvy0nMKJBY5N2_gyjW4pQ96aXO_jJAZ"
+                  />
+                </Box>
+                <Text style={textStyle}>Bulbous and tuberous plants</Text>
+              </VStack>
             </Button>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"30px"}
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUKAUVYDZcGmZalC3Q0zwyiqFjtjCQSV-6feR9tqgTFVnvRQL-"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Perennials
-                </Text>
-              </Box>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Shrubs">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"30px"}
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUKAUVYDZcGmZalC3Q0zwyiqFjtjCQSV-6feR9tqgTFVnvRQL-"
+                  />
+                </Box>
+                <Text style={textStyle}>Perennials</Text>
+              </VStack>
             </Button>
           </VStack>
-          <VStack spacing={4}>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"35px"}
-                  bg={"white"}
-                  src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT3WgIseKpDjHQAdz3EjP_ukjiL3L3NCXWVUAjIH0saihMG21-u"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Climbing plants
-                </Text>
-              </Box>
+          <VStack spacing={5}>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Climbers">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"35px"}
+                    bg={"white"}
+                    src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT3WgIseKpDjHQAdz3EjP_ukjiL3L3NCXWVUAjIH0saihMG21-u"
+                  />
+                </Box>
+                <Text style={textStyle}>Climbing plants</Text>
+              </VStack>
             </Button>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"30px"}
-                  src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSFDhAkMfqUwldFTOETFDtiKiERoYxr6glnQh-GiCmaqPBsRRIm"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Roses
-                </Text>
-              </Box>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Rosen">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"32px"}
+                    src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSFDhAkMfqUwldFTOETFDtiKiERoYxr6glnQh-GiCmaqPBsRRIm"
+                  />
+                </Box>
+                <Text style={textStyle}>Roses</Text>
+              </VStack>
             </Button>
           </VStack>
-          <VStack spacing={4}>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"40px"}
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLHiRdn_Gk2tyfBumWkr410oYPEMHzNMi8S98c7fnw0Jq01p7"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Container plants
-                </Text>
-              </Box>
+          <VStack spacing={5}>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Container plants">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"45px"}
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLHiRdn_Gk2tyfBumWkr410oYPEMHzNMi8S98c7fnw0Jq01p7"
+                  />
+                </Box>
+                <Text style={textStyle}>Container plants</Text>
+              </VStack>
             </Button>
 
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"27px"}
-                  src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRi-YQCxjs6hI7Hy2Ov6YtrXDaVxQ3B-5GH6e-nS2W_4OHR2Q6_"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Summer flowers
-                </Text>
-              </Box>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Summer flowers">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"27px"}
+                    src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRi-YQCxjs6hI7Hy2Ov6YtrXDaVxQ3B-5GH6e-nS2W_4OHR2Q6_"
+                  />
+                </Box>
+                <Text style={textStyle}>Summer flowers</Text>
+              </VStack>
             </Button>
           </VStack>
-          <VStack spacing={4}>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"30px"}
-                  src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQBa-rj3lOmykg-VynDZ_IZDVFz391Db6pnz7rqepTwLucCO0c2"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Fruit
-                </Text>
-              </Box>
+          <VStack spacing={5}>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Obst">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"30px"}
+                    src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQBa-rj3lOmykg-VynDZ_IZDVFz391Db6pnz7rqepTwLucCO0c2"
+                  />
+                </Box>
+                <Text style={textStyle}>Fruit</Text>
+              </VStack>
             </Button>
 
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"40px"}
-                  src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTHQi876CLQzk4hTF5QM93tJM91OVEzAjdN6C9RzvKQMzRc0zPP"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Trees and Shrubs
-                </Text>
-              </Box>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Trees and shrubs">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"40px"}
+                    src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTHQi876CLQzk4hTF5QM93tJM91OVEzAjdN6C9RzvKQMzRc0zPP"
+                  />
+                </Box>
+                <Text style={textStyle}>Trees and Shrubs</Text>
+              </VStack>
             </Button>
           </VStack>
-          <VStack spacing={4}>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"30px"}
-                  src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQdObUXjJASq_-YXFStG8eGWNoVb2U8HI-EADMk2PfzONgeKuSb"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Herbs
-                </Text>
-              </Box>
+          <VStack spacing={5}>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Herbs">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"25px"}
+                    src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQdObUXjJASq_-YXFStG8eGWNoVb2U8HI-EADMk2PfzONgeKuSb"
+                  />
+                </Box>
+                <Text style={textStyle}>Herbs</Text>
+              </VStack>
             </Button>
 
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
-                <Image
-                  m={"0px"}
-                  p={"0px"}
-                  width={"40px"}
-                  src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ5XWllOEVhi3uTuOGleUuvYFZb2dZRnAaEYf-vI0wSrrdNauXc"
-                />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Vegetables
-                </Text>
-              </Box>
+            <Button style={btnStyle}>
+              <VStack spacing={0}>
+                <Box as="span" title="Vegetables">
+                  <Image
+                    m={"0px"}
+                    p={"0px"}
+                    width={"35px"}
+                    src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ5XWllOEVhi3uTuOGleUuvYFZb2dZRnAaEYf-vI0wSrrdNauXc"
+                  />
+                </Box>
+                <Text style={textStyle}>Vegetables</Text>
+              </VStack>
             </Button>
           </VStack>
-          <HStack>
-            <Button background={"none"} cursor={"pointer"} border={"none"}>
-              <Box>
+
+          <Button style={btnStyle}>
+            <VStack spacing={0}>
+              <Box as="span" title="Houseplants">
                 <Image
                   m={"0px"}
                   p={"0px"}
                   width={"20px"}
                   src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRIPt9b6IL_mWPhAYpAyF56uwx5u80Famxn2vrupPbZ85V6etU2"
                 />
-                <Text
-                  mt={"0px"}
-                  p={"0px"}
-                  fontFamily={"Roboto(woff2),sans-serif"}
-                  lineHeight={"17px"}
-                  verticalAlign={"baseline"}
-                  fontWeight={700}
-                >
-                  Houseplants
-                </Text>
               </Box>
-            </Button>
-          </HStack>
+              <Text style={textStyle}>Houseplants</Text>
+            </VStack>
+          </Button>
         </Flex>
+      </Box>
+      <Box bgColor={"#F5F5F5"}>
+        <Box width={"75%"} margin={"auto"} p={"30px 0px"}>
+          <Text
+            textAlign={"left"}
+            lineHeight={"32px"}
+            fontSize={"19px"}
+            fontWeight={700}
+            fontStretch={"100%"}
+            text-decoration={"none solid rgb(64, 64, 64)"}
+          >
+            Filter by Plant
+          </Text>
+        </Box>
       </Box>
     </Box>
   );
