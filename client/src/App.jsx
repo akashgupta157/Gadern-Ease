c707ac6a1ec3b5e85afc2f9e615b3eea451ad30a
Juri Kalita <jurikalita011@gmail.com>
2023-08-25 22:21:45 +0530

    added day4 tasks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Allroutes from "./Allroutes";
 import { Navbar } from "./components/Navbar";
 import { useLocation } from "react-router-dom";
+import { Plants } from "./pages/Plants";
 export default function App() {
   const path = useLocation();
   return (
     <div>
-      {path.pathname === "/login" || path.pathname === "/register" ? null : (
+      {/* {path.pathname === "/login" || path.pathname === "/register" ? null : (
         <Navbar />
       )}
-      <Allroutes />
+      <Allroutes /> */}
+      <Plants />
     </div>
   );
 }
