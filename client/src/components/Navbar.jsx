926f4c016697e6def1e5e7722b4328826e26f2b6
SatyaIndraDev <satyaindrad@gmail.com>
2023-08-25 22:02:20 +0530

    d4

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,44 +1,56 @@
-const mystyle = {
-  display: "flex",
-  justifyContent: "space-between",
-  textAlign: "center",
-  fontSize: "23px",
-  fontWeight: "bold",
-  backgroundColor: "#F5F5F5",
-  boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-  fontFamily: " Roboto, sans-serif ",
-  color: "#404040",
-  fontWeight: "700",
-  position: "sticky",
-  top: "0",
-};
-const allname = {
-  margin: "34px",
-};
+import { Link } from "react-router-dom";
+    const mystyle = {
+        display:"flex" ,justifyContent :"space-between", 
+        textAlign:"center",fontSize:"15px" ,
+        fontWeight: 'bold' ,backgroundColor :"#F5F5F5" ,
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" , 
+        fontFamily:" Roboto, sans-serif ",
+        color :"#404040",
+        position: "sticky",
+  top: "0"
+  ,
+    }
+    const allname={
+        margin:"24px",
+        textAlign:"center",fontSize:"15px" ,
+        fontWeight: 'bold' ,backgroundColor :"#F5F5F5" ,
+        fontFamily:" Roboto, sans-serif ",
+        color :"#404040",
+    }
 
 export const Navbar = () => {
-  return (
-    <div>
-      {" "}
-      <div style={mystyle}>
-        <div>
-          {" "}
-          <img src="logo.jpeg" alt="" style={{ width: "350px" }} />
-        </div>
-        <div style={allname}>Gardening Practice</div>
-        <div style={allname}>Houseplants</div>
-        <div style={allname}>Perennials</div>
-        <div style={allname}>Trees & Shrubs </div>
-        <div style={allname}>More Plants </div>
-        <div style={allname}>Topics</div>
-      </div>
-      <div style={{ margin: "auto" }}>
-        <img
-          src="newa.jpg"
-          alt=""
-          style={{ width: "1000px ", margin: "auto" }}
-        />
-      </div>
-    </div>
-  );
-};
+
+
+ return <div style={mystyle}>
+    <div> <img src="logo.jpeg" alt="" style={{width:"250px"}}/></div>
+
+    <Link to="/" style={{textDecoration: 'none'  , color:"black"}}>
+      <h3 data-testid="home-link"  style={allname}>Home </h3>
+    </Link>
+    <Link to="/signup" style={{textDecoration: 'none'  , color:"black"}}>
+      <h3 data-testid="home-link"  style={allname}>Gardening Practice</h3>
+    </Link>
+    <Link to="/signup" style={{textDecoration: 'none'  , color:"black"}}>
+      <h3 data-testid="home-link"  style={allname}>Plants</h3>
+    </Link>
+    <Link to="/signup" style={{textDecoration: 'none'  , color:"black"}}>
+      <h3 data-testid="home-link"  style={allname}>Perennials</h3>
+    </Link>
+    <Link to="/signup" style={{textDecoration: 'none'  , color:"black"}}>
+      <h3 data-testid="home-link"  style={allname}>Trees & Shrubs</h3>
+    </Link>
+    
+    <Link to="/signup" style={{textDecoration: 'none'  , color:"black"}}>
+      <h3 data-testid="home-link"  style={allname}>Topics</h3>
+    </Link>
+
+<Link to="/signup" style={{textDecoration: 'none'  , color:"black"}}>
+      <h3 data-testid="home-link"  style={allname}>SignUp</h3>
+    </Link>
+
+
+ </div>
+ 
+
+}
+
