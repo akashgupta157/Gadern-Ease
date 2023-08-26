b0918fac7e21da6a56f4de67033d75417dfde1bd
Juri Kalita <jurikalita011@gmail.com>
2023-08-26 20:25:44 +0530

    added day5 tasks

--- client/src/Allroutes.jsx
+++ client/src/Allroutes.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import { Register } from "./pages/Register";
 import BlogPost from "./pages/BlogPost";
+import { Plants } from "./pages/Plants";
 export default function Allroutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/blog_post" element={<BlogPost />} />
+      <Route path="/plants" element={<Plants />} />
     </Routes>
   );
 }
