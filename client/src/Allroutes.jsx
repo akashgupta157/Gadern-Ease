9576c91be62903ea87d8ecb6f0266dec7bcf9a28
Your Name <you@example.com>
2023-08-27 22:45:45 +0530

    update

--- client/src/Allroutes.jsx
+++ client/src/Allroutes.jsx
@@ -6,13 +6,19 @@ import BlogPost from "./pages/BlogPost";
 import { Plants } from "./pages/Plants";
 import SinglePlantPage from "./components/SinglePlantPage";
 import { Home } from "./components/Home";
+import { useSelector } from "react-redux";
 export default function Allroutes() {
+  const auth = useSelector((state) => state.authReducer.isAuthenticated);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/blog_post" element={<BlogPost />} />
+      {auth ? (
+        <Route path="/blog_post" element={<BlogPost />} />
+      ) : (
+        <Route path="/" element={<Home />} />
+      )}
       <Route path="/plants" element={<Plants />} />
       <Route path="/plants/:id" element={<SinglePlantPage />} />
     </Routes>
