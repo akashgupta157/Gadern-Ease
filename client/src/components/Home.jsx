e2390ef6892379f2fee1ddf02fee1e547af88c7e
Akash Gupta <akashgupta989322@gmail.com>
2023-10-13 17:08:23 +0530

    update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,105 +1,131 @@
+import React from "react";
 import "./Home.css";
-
-export const Home = () => {
-  const buttonprop = {
-    marginLeft: "300px",
-    marginTop: "100px",
-    backgroundColor: "#4CAF50",
-    border: "none",
-    color: "white",
-    padding: "15px 32px",
-    textAlign: "center",
-    textDecoration: "none",
-    display: "inline-block",
-    fontSize: "16px",
-    borderRadius: "10px",
-  };
-
-  const highlight = {
-    color: "#404040",
-    fontSize: "15px",
-    fontWeight: "bold",
-  };
-
+export function Home() {
+  const blogPosts = [
+    {
+      title:
+        "Attend Your Local Orchid Society Meeting for Tons of Great Information",
+      description:
+        "Learn about the benefits of attending local orchid society meetings and the wealth of information they provide.",
+      image: "https://live.staticflickr.com/1486/25710684284_d1e3207221_b.jpg",
+    },
+    {
+      title:
+        "Underplanting. Can It Be Done With Houseplants and What Plants are Best?",
+      description:
+        "Discover the concept of underplanting with houseplants and the best plant choices for adding interest to large planters.",
+      image: "https://live.staticflickr.com/2635/4037410109_9964bdc8a7_b.jpg",
+    },
+    {
+      title:
+        "Fern Fronds Have Spores on their Undersides. What Is Their Purpose?",
+      description:
+        "Explore the spores found on the undersides of fern fronds and their role, often mistaken for scale insects.",
+      image:
+        "https://c8.alamy.com/comp/2D38AKT/nasturtium-leaves-of-different-green-tones-natural-plant-background-2D38AKT.jpg",
+    },
+    {
+      title:
+        "Why is Your Orchid Leaf Wrinkled and Pleated and Can It Be Fixed?",
+      description:
+        "Find out the reasons behind wrinkled and pleated orchid leaves and whether it can be remedied.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKla_5HDGeFptiJL_bIcBwb906SIB4d2-jA",
+    },
+    {
+      title:
+        "The Pollen Sacs are Missing on My African Violet Flowers. What Happened?",
+      description:
+        "Learn about the absence of pollen sacs on African violet flowers and why it's a positive development.",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZk5G0Ayf_5O_olT-hoEXBfN6Sfyjxv9lNHA&usqp=CAU",
+    },
+    {
+      title:
+        "What Plant Leaves Are Telling You About How To Care for The Plant",
+      description:
+        "Discover how plant leaves communicate their care needs and how to interpret their signals.",
+      image:
+        "https://m.media-amazon.com/images/I/5150sp+qWUL._AC_UF1000,1000_QL80_.jpg",
+    },
+  ];
   return (
-    <div>
-      <div className="images1">
-        <img
-          src="newa.jpg"
-          alt=""
-          style={{ width: "100% ", height: "400px", objectFit: "cover" }}
-        />
-        <h3
-          style={{
-            paddingLeft: "20px",
-            color: "#404040",
-            fontSize: "15px",
-            fontWeight: "bold",
-            paddingTop: "10px",
-          }}
-        >
-          GREEN LIFE
-        </h3>
-        <h6 style={{ paddingLeft: "20px" }}>
-          Making lavender oil: Preserve the scent of the south
-        </h6>
-        <h4 style={{ paddingLeft: "20px" }}>
-          Do you love lavender? With these simple instructions, you can
-          transform these fragrant blossoms from your garden into aromatic
-          lavender oil.
-        </h4>
-        <button className="buttonprop" style={{ margin: "20px 20px" }}>
-          Learn more
-        </button>
-      </div>
-
-      <div className="main1">
-        <div className="div1">
-          <div>
-            <img src="1.jpg" alt="" style={{ borderRadius: "10px" }} />
-          </div>
-          <div style={{ padding: "40px" }}>
-            <h3 style={highlight}>ORNAMENTAL GARDEN</h3>
-            <h1>How to fertilize Hydrangeas</h1>
-            <button className="buttonprop">Learn more</button>
-          </div>
-        </div>
-        <div className="div1">
-          <div>
-            <img src="2.jpg" alt="" style={{ borderRadius: "10px" }} />
-          </div>
-          <div style={{ padding: "40px" }}>
-            <h3>ORNAMENTAL GARDEN</h3>
-            <h1>How to fertilize Hydrangeas</h1>
-            <button className="buttonprop">Learn more</button>
-          </div>
-        </div>
-
-        <div className="div1">
-          <div>
-            <img src="3.jpg" alt="" style={{ borderRadius: "10px" }} />
-          </div>
-          <div style={{ padding: "40px" }}>
-            <h3>ORNAMENTAL GARDEN</h3>
-            <h1>How to fertilize Hydrangeas</h1>
-            <button className="buttonprop">Learn more</button>
-          </div>
-        </div>
-      </div>
-
-      <h1 style={{ textAlign: "Center", color: "green" }}>Plants</h1>
-
-      <div className="last">
+    <>
+      <div id="banner">
         <div>
-          <img src="m1.jpg" alt="" className="lastimg" />
+          <h1>Grow your green thumb.</h1>
+          <p>
+            What’s the best way to nurture your green thumb when you don’t have
+            a yard or space? Houseplants! This is your guide to raising the
+            best, healthiest, and happiest potted plants nature has ever seen.
+          </p>
         </div>
-        <div style={{ display: "grid", gridTemplateColumns: "repeat(2,1fr)" }}>
-          <img src="m2.jpg" alt="" className="imglast2" />
-          <img src="m3.jpg" alt="" className="imglast2" />
-          <img src="m4.jpg" alt="" className="imglast2" />
-          <img src="m5.jpg" alt="" className="imglast2" />
+        <img
+          src="https://www.epicgardening.com/wp-content/uploads/2023/06/home-banner.jpg"
+          alt=""
+        />
+      </div>
+      <div id="mid">
+        <div>
+          <h2>Grow beautiful houseplants!</h2>
+          <p>
+            The GardenEase is all about helping you find success in growing
+            beautiful, thriving houseplants.
+          </p>
+        </div>
+        <section>
+          <div>
+            <img
+              src="https://thehouseplantguru.com//wp-content/uploads/2019/04/plant-1.png"
+              alt=""
+            />
+            <h3>Healthy, thriving houseplants</h3>
+            <p>
+              Learn about your favorite houseplants: Do you need fertilizer? How
+              much should you water them? Is the plant failing? Is it beyond
+              help? Find the answers to these questions and more!
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://thehouseplantguru.com//wp-content/uploads/2019/04/watering-can.png"
+              alt=""
+            />
+            <h3>Practical tips & tools</h3>
+            <p>
+              We’ll talk about my absolute favorite tips & tools! From being
+              able to tell when plants really need water, to selecting the best
+              plants for the light in your home, you’ll leave my blog with the
+              knowledge you need to succeed with your houseplants.
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://thehouseplantguru.com//wp-content/uploads/2019/04/leaf.png"
+              alt=""
+            />
+            <h3>Dispell common myths!</h3>
+            <p>
+              You can’t believe everything you read on the internet! Should I
+              use mayonnaise to shine my plants’ leaves? Do I need pebbles at
+              the bottom of my pots for drainage? Can I get the leaves of my
+              African violet wet? We’ll solve every mystery together!
+            </p>
+          </div>
+        </section>
+      </div>
+      <div id="end">
+        <h2>Articles and Blog</h2>
+        <div>
+          {blogPosts.map((e) => (
+            <div>
+              <img src={e.image} alt={e.title} />
+              <h3>{e.title}</h3>
+              <p>{e.description}</p>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   );
-};
+}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,8 @@
-import "./Navbar.css";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-const allname = {
-  margin: "24px",
-  textAlign: "center",
-  fontSize: "15px",
-  fontWeight: "bold",
-  backgroundColor: "#F5F5F5",
-  fontFamily: " Roboto, sans-serif ",
-  color: "#404040",
-};
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./navbar.css";
+import logo from "../assets/logo.png";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import {
   Menu,
   MenuButton,
@@ -18,74 +11,71 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { logout } from "../redux/authReducer/action";
+import { useDispatch, useSelector } from "react-redux";
 export const Navbar = () => {
-  {
-    /* <div style={mystyle}> */
-  }
+  const [showNavbar, setShowNavbar] = useState(false);
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
   const auth = useSelector((state) => state.authReducer);
   const nav = useNavigate();
   const dispatch = useDispatch();
   const toast = useToast();
   return (
-    <div className="mystyle">
-      <div>
-        {" "}
-        <img
-          onClick={() => nav("/")}
-          src="logo.jpeg"
-          alt=""
-          style={{ width: "250px", cursor: "pointer" }}
-        />
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img
+            src={logo}
+            alt=""
+            style={{ width: "250px", cursor: "pointer" }}
+          />
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <HamburgerIcon />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/plants">Gardening Practice</NavLink>
+            </li>
+            <li>
+              {auth.isAuthenticated ? (
+                <Menu>
+                  <MenuButton sx={{ fontWeight: "bold", color: "green" }}>
+                    Hi {auth?.user.user.name}
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem onClick={() => nav("/blog_post")}>
+                      Add Blogs
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        nav("/");
+                        dispatch(logout());
+                        toast({
+                          title: `Logged out successful`,
+                          status: "success",
+                          position: "top-center",
+                          duration: 2000,
+                          isClosable: true,
+                        });
+                      }}
+                    >
+                      Logout
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              ) : (
+                <NavLink to="/register">Signup</NavLink>
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
-
-      <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-        <h3 style={allname}>Home </h3>
-      </Link>
-      <Link to="/plants" style={{ textDecoration: "none", color: "black" }}>
-        <h3 style={allname}>Gardening Practice</h3>
-      </Link>
-      <Link to="/plants" style={{ textDecoration: "none", color: "black" }}>
-        <h3 style={allname}>Plants</h3>
-      </Link>
-      <Link to="/plants" style={{ textDecoration: "none", color: "black" }}>
-        <h3 style={allname}>Perennials</h3>
-      </Link>
-      <Link to="/plants" style={{ textDecoration: "none", color: "black" }}>
-        <h3 style={allname}>Trees & Shrubs</h3>
-      </Link>
-
-      <Link to="/plants" style={{ textDecoration: "none", color: "black" }}>
-        <h3 style={allname}>Topics</h3>
-      </Link>
-      {auth.isAuthenticated ? (
-        <Menu>
-          <MenuButton sx={{ fontWeight: "bold", color: "green" }}>
-            Hi {auth?.user.user.name}
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={() => nav("/blog_post")}>Add Blogs</MenuItem>
-            <MenuItem
-              onClick={() => {
-                nav("/");
-                dispatch(logout());
-                toast({
-                  title: `Logged out successful`,
-                  status: "success",
-                  position: "top-center",
-                  duration: 2000,
-                  isClosable: true,
-                });
-              }}
-            >
-              Logout
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      ) : (
-        <Link to="/register" style={{ textDecoration: "none", color: "black" }}>
-          <h3 style={allname}>SignUp</h3>
-        </Link>
-      )}
-    </div>
+    </nav>
   );
 };
